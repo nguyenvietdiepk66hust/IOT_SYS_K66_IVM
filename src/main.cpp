903f5020f863dc1c0499558cb8697dbe231752b6
nguyenvietdiepk66hust <134980440+nguyenvietdiepk66hust@users.noreply.github.com>
2024-11-17 19:46:26 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -44,11 +44,9 @@ void taskReadSensor(void *pvParameters) {
         vTaskDelay(1 / portTICK_PERIOD_MS);  // Adjust as needed
     }
 }
-
 // Task 2: Write data to SD card
 void taskWriteSDCard(void *pvParameters) {
     SensorData sensorData;
-
     // Cấu hình GPIO
     gpio_set_pull_mode((gpio_num_t)15, GPIO_PULLUP_ONLY); // D3
     gpio_set_pull_mode((gpio_num_t)2, GPIO_PULLUP_ONLY);  // D0
@@ -65,23 +63,17 @@ void taskWriteSDCard(void *pvParameters) {
     sdmmc_card_t *card;
     const char mount_point[] = "/sdcard";
     ESP_LOGI(TAG, "Khởi tạo SDMMC...");
-
     sdmmc_host_t host = SDMMC_HOST_DEFAULT();
     sdmmc_slot_config_t slot_config = SDMMC_SLOT_CONFIG_DEFAULT();
-
+    adc1_config_width(ADC_WIDTH_BIT_12);
+    adc1_config_channel_atten(ADC1_CHANNEL_0, ADC_ATTEN_DB_11); // Đọc từ GPIO5
     esp_err_t ret = esp_vfs_fat_sdmmc_mount(mount_point, &host, &slot_config, &mount_config, &card);
     if (ret != ESP_OK) {
         ESP_LOGE(TAG, "Không thể gắn thẻ nhớ: %s", esp_err_to_name(ret));
         return;
     }
-
     ESP_LOGI(TAG, "Đã gắn thẻ nhớ, thông tin:");
     sdmmc_card_print_info(stdout, card);
-
-    // Cấu hình ADC
-    adc1_config_width(ADC_WIDTH_BIT_12);
-    adc1_config_channel_atten(ADC1_CHANNEL_0, ADC_ATTEN_DB_11); // Đọc từ GPIO5
-
     while (1) {
         if (xQueueReceive(dataQueue, &sensorData, portMAX_DELAY) == pdTRUE) {
             FILE* file = fopen("/sdcard/data.txt", "a");
@@ -96,8 +88,6 @@ void taskWriteSDCard(void *pvParameters) {
     }
     vTaskDelay(0.5/portTICK_PERIOD_MS);
 }
-
-
 // Task 3: Send data to Modbus Client and clear SD card when done
 
 void taskSendToModbus(void *pvParameters) {
